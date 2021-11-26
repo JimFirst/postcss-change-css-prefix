a2c0411f815954f355a160c54facef124486e620
董铭杰 <dongmj@getui.com>
2021-11-26 14:23:52 +0800

    [init]:v 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # postcss-change-css-prefix
 a postcss plugin to change element-ui prefix
+
+### 快速上手
+
+```
+npm i postcss-change-css-prefix -D
+
+
+<!-- postcss.config.js -->
+const addCssPrefix = require('postcss-change-css-prefix')
+
+module.exports = {
+  plugins: [
+    addCssPrefix({
+      prefix: 'el-',
+      replace: 'gp-',
+    }),
+  ],
+}
+
+```
+
+### options
+
+1. prefix
+    * type: string, 可选
+    * 需要被替换的前缀
+    * default: el-
+
+2. replace
+    * type: string, 可选
+    * 需要替换的前缀
+    * default: gt-
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const postcss = require('postcss')
+module.exports = postcss.plugin('postcss-change-css-prefix', function (opts = {}) {
+  const { prefix = 'el-', replace = 'gt-' } = opts || {}
+  
+  // 接收两个参数，第一个是每个css文件的ast，第二个参数中可获取转换结果相关信息(包括当前css文件相关信息)
+  function plugin(css, result) {
+    css.walkRules((rule) => {
+      // 遍历当前ast所有rule节点
+      const { selector } = rule
+      if (
+        selector.includes(prefix) &&
+        !selector.includes(replace)
+      ) {
+        const clone = rule.clone()
+        const reg = new RegExp(`(^|(\\s)*|(\.))${prefix}(?!icon)`, 'g')
+        clone.selector = selector.replace(reg, `$1${replace}`)
+        rule.replaceWith(clone)
+      }
+    })
+  }
+
+  return plugin
+})
